9a490cd8bd1cd182adf523e1fbb8c7827c0aca5f
wangli <wangli_cq@hotmail.com>
2016-12-21 20:20:40 +0800

    [Li]add redPacket test

--- src/test/java/com/templateMatch/test/TemplateMatching.java
+++ src/test/java/com/templateMatch/test/TemplateMatching.java
@@ -14,6 +14,10 @@ public class TemplateMatching {
 
     Matching matching =new Matching();
 
+    @Test
+    public void FindRedPacket() throws Exception {
+        int location[] = matching.getMatchingLocation1("screenshots/wechat.png", "temp/tem-wechat.png", "compare/recpacket_compared.png");
+    }
 
     @Test
     public void FindLenaFace() throws Exception {
