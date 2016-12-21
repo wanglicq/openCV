782828d9e2689ff658fbdd854e2926d621682ba7
wangli <wangli_cq@hotmail.com>
2016-12-21 19:50:21 +0800

    [Li]Rename

---
+++ src/test/java/com/templateMatch/test/TemplateMatching.java
@@ -0,0 +1,30 @@
+package com.templateMatch.test;
+
+
+import org.junit.Test;
+import org.opencv.core.Core;
+
+/**
+ * Created by wangli on 21/12/2016.
+ */
+public class TemplateMatching {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    Matching matching =new Matching();
+
+    @Test
+    public void FindRedPacket() throws Exception {
+        int location[] = matching.getMatchingLocation1("screenshots/wechat.png", "temp/tem-wechat.png", "compare/recpacket_compared.png");
+    }
+
+    @Test
+    public void FindLenaFace() throws Exception {
+        int location[] = matching.getMatchingLocation1("screenshots/lena.png", "temp/temp_lena_face.png", "compare/recpacket_compared.png");
+
+        System.out.println("x= "+location[0]+",  y="+location[1]);
+    }
+
+
+}
